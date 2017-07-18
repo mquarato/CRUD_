e7e220105e09d48c60c06ccde3d370df20cf6dad
mquarato <mq451@stern.nyu.edu>
2017-07-17 21:08:22 -0400

    Added git ignore

---
+++ app/products_app.py
@@ -0,0 +1,8 @@
+
+print("-----------------------------------")
+print("PRODUCTS APPLICATION")
+print("-----------------------------------")
+print("Welcome @")
+
+print("There are 21 products in the database. Please select an operation:")
+print("\n")
