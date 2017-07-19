9c50451bfbd23d4451afc53bf234dc058a5614e0
mquarato <mq451@stern.nyu.edu>
2017-07-19 18:08:35 -0400

    Checkpoint 1 - prompting to choose an operation

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# CRUD_
+# Production Application Database
+
+This application allows you to use a database of products to do the following functions:
+
+1) List all the items in the Database
+2) Show a specific item's information via the item ID
+3) Create a new item and store it in the Database
+4) Update existing items in the Database
+5) Destroy (Delete) items in the Database
+
+#Installation
+
+Download the source code:
+
+```shell
+git clone remote_clone_address_of_your_repo_here
+cd some/path/to/repo/
+```
+
+Finally, download the [example `products.csv` file]
+(https://raw.githubusercontent.com/prof-rossetti/nyu-info-2335-70-201706/master/projects/crud-app/products.csv) and save it as `data/products.csv`.
+
+## Usage
+
+```shell
+python app/products_app.py
+```
--- app/products_app.py
+++ app/products_app.py
@@ -1,8 +1,36 @@
+import csv
+
+csv_file_path = "products.csv"
 
 print("-----------------------------------")
 print("PRODUCTS APPLICATION")
 print("-----------------------------------")
-print("Welcome @")
-
+print("Welcome @mquarato!")
+print("\n")
 print("There are 21 products in the database. Please select an operation:")
 print("\n")
+print("   operation | description")
+print("   --------- | --------------------")
+print("   'List'    | Display a list of product identifiers and names.")
+print("   'Show'    | Show information about a product.")
+print("   'Create'  | Add a new product.")
+print("   'Update'  | Edit an existing product")
+print("   'Destroy' | Delete an existing product.")
+
+
+while True:
+    product_operation = input("")
+    if product_operation == "List":
+        with open(csv_file_path, "r") as csv_file:
+            reader = csv.DictReader(csv_file) # assuming your CSV has headers, otherwise... csv.reader(csv_file)
+            print("There are 20 products:")
+            for row in reader:
+                print(" + " + row["name"])
+    if product_operation == "Show":
+        product_id = input("OK. Please specify the product's identifier: ")
+        print("SHOWING A PRODUCT HERE!")
+        def lookup_product_by_id(product_id):
+            matching_products = [product for product in products if product["id"] == product_id]
+            return matching_products[0]
+    else:
+        break
