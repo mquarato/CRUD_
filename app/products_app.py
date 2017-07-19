a4a1e2749c8483f9a6853bce3a2d1fb0db5f03a3
mquarato <mq451@stern.nyu.edu>
2017-07-19 18:53:09 -0400

    Handle user inputs

--- app/products_app.py
+++ app/products_app.py
@@ -1,6 +1,6 @@
 import csv
 
-csv_file_path = "products.csv"
+csv_file_path = "data/products.csv"
 
 print("-----------------------------------")
 print("PRODUCTS APPLICATION")
@@ -20,17 +20,25 @@ print("   'Destroy' | Delete an existing product.")
 
 while True:
     product_operation = input("")
+    print("This is your desired operation: " + product_operation)
     if product_operation == "List":
         with open(csv_file_path, "r") as csv_file:
             reader = csv.DictReader(csv_file) # assuming your CSV has headers, otherwise... csv.reader(csv_file)
             print("There are 20 products:")
             for row in reader:
-                print(" + " + row["name"])
-    if product_operation == "Show":
+                print(" + " + str(row))
+    elif product_operation == "Show":
         product_id = input("OK. Please specify the product's identifier: ")
         print("SHOWING A PRODUCT HERE!")
         def lookup_product_by_id(product_id):
-            matching_products = [product for product in products if product["id"] == product_id]
+            matching_products = [product for product in products if row["id"] == product_id]
             return matching_products[0]
+    elif product_operation == "Create":
+        print("CREATING A NEW PRODUCT:")
+    elif product_operation == "Update":
+        print("UPDATING A PRODUCT:")
+    elif product_operation == "Destroy":
+        print("REMOVING A PRODUCT:")
     else:
+        print("PLEASE CHOOSE ONE OF THE AVAILABLE OPERATIONS!")
         break
