0e810b493ccea84640b3d9b9924cb893a208eb58
mquarato <mq451@stern.nyu.edu>
2017-07-19 19:58:01 -0400

    show number of products in menu

--- app/products_app.py
+++ app/products_app.py
@@ -16,29 +16,52 @@ print("   'Show'    | Show information about a product.")
 print("   'Create'  | Add a new product.")
 print("   'Update'  | Edit an existing product")
 print("   'Destroy' | Delete an existing product.")
-
+print("\n")
 
 while True:
-    product_operation = input("")
+    product_operation = input("\nInput your operation: ")
+    product_operation = product_operation.title()
     print("This is your desired operation: " + product_operation)
-    if product_operation == "List":
+    def list_products():
         with open(csv_file_path, "r") as csv_file:
-            reader = csv.DictReader(csv_file) # assuming your CSV has headers, otherwise... csv.reader(csv_file)
+            reader = csv.DictReader(csv_file)
             print("There are 20 products:")
             for row in reader:
-                print(" + " + str(row))
+                print(" + ", dict(row))
+
+    def show_products():
+        print("SHOWING A PRODUCT HERE:")
+
+    def create_products():
+        print("CREATING A NEW PRODUCT:")
+
+    def product_operation():
+        print("UPDATING A PRODUCT:")
+
+    def destroy_operation():
+        print("REMOVING A PRODUCT:")
+
+    if product_operation == "List":
+        list_products()
+        # with open(csv_file_path, "r") as csv_file:
+        #     reader = csv.DictReader(csv_file)
+        #     print("There are 20 products:")
+        #     for row in reader:
+        #         print(" + ", dict(row))
     elif product_operation == "Show":
         product_id = input("OK. Please specify the product's identifier: ")
-        print("SHOWING A PRODUCT HERE!")
         def lookup_product_by_id(product_id):
             matching_products = [product for product in products if row["id"] == product_id]
             return matching_products[0]
+        #for product_ids in product_id:
+        #    product = lookup_product_by_id(product_id)
+        #    print(product)
     elif product_operation == "Create":
-        print("CREATING A NEW PRODUCT:")
+        create_products()
     elif product_operation == "Update":
-        print("UPDATING A PRODUCT:")
+        product_operation()
     elif product_operation == "Destroy":
-        print("REMOVING A PRODUCT:")
+        destroy_operation()
     else:
         print("PLEASE CHOOSE ONE OF THE AVAILABLE OPERATIONS!")
         break
