b433bd4a4770dcded315298f449e87f6581bfbb5
mquarato <mq451@stern.nyu.edu>
2017-07-23 23:47:00 -0400

    Completed Destroy and part of Update

--- app/products_app.py
+++ app/products_app.py
@@ -1,4 +1,5 @@
 import csv
+import operator
 
 csv_file_path = "data/products.csv"
 other_path = "data/other_products.csv"
@@ -13,6 +14,8 @@ with open(csv_file_path, "r") as csv_file:
 def lookup_product_by_id(product_id):
     matching_products = [product for product in products if product["id"] == product_id]
     return matching_products[0]
+
+products = sorted(products, key=operator.itemgetter("id"))
 
 print("-----------------------------------")
 print("PRODUCTS APPLICATION")
@@ -63,25 +66,35 @@ def update_product():
         for product in products:
             product_show = lookup_product_by_id(product_id)
         print("SHOWING A PRODUCT HERE: ", dict(product_show))
-
+        row = int(product_id)
         product_name = input("name is: ")
         product_aisle = input("aisle is: ")
         product_department = input("department is: ")
         product_price = input("price is: ")
         updated_product = {
-            "id": int(product_id),
+            "id": product_id,
             "name": product_name,
             "aisle": product_aisle,
             "department": product_department,
             "price": product_price,
         }
-        print("NEW PRODUCT IS: ", updated_product)
+        del products[row-1]
+        products.append(updated_product)
+        print("UPDATED PRODUCT IS: ", updated_product)
     else:
         print("ERROR, Invalid product ID!")
 
 
 def destroy_operation():
-    print("REMOVING A PRODUCT:")
+    product_id = input("Please specify the product's identifier: ")
+    if int(product_id) <= len(products):
+        for product in products:
+            product_show = lookup_product_by_id(product_id)
+        print("REMOVING A PRODUCT: ", dict(product_show))
+        row = int(product_id)
+        del products[row-1]
+    else:
+        print("ERROR, Invalid product ID!")
 
 #while True:
 product_operation = input("\nInput your operation: ")
@@ -95,6 +108,7 @@ elif product_operation == "Update": update_product()
 elif product_operation == "Destroy": destroy_operation()
 else:
     print("PLEASE CHOOSE ONE OF THE AVAILABLE OPERATIONS!")
+
 
 other_path = "data/other_products.csv"
 with open(other_path, "w") as csv_file:
